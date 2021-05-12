24e12a99236894aba5365c2281a2e9b01d72b1ce
liyq <liyaqi@cdeledu.com>
2021-05-12 17:16:42 +0800

    refactor: 添加ts支持

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,11 +1,11 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin }= require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   mode: "development",
 
-  entry: "./src/index.js",
+  entry: "./src/index.tsx",
 
   output: {
     filename: "bundle.js",
@@ -15,42 +15,44 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        loader: "babel-loader",
+        test: /\.tsx$/i,
+        loader: "ts-loader",
       },
       {
         test: /\.(css|less)$/,
         use: [
           {
-            loader: "style-loader"
+            loader: "style-loader",
           },
           {
-            loader: "css-loader"
+            loader: "css-loader",
           },
           {
             loader: "less-loader",
             options: {
               lessOptions: {
-                javascriptEnabled: true
-              }
-            }
-          }
-        ]
-      }
+                javascriptEnabled: true,
+              },
+            },
+          },
+        ],
+      },
     ],
+  },
+
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
   },
 
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: "./src/index.html"
-    })
+      template: "./src/index.html",
+    }),
   ],
 
   devServer: {
     port: 3000,
     contentBase: path.resolve(__dirname, "dist"),
   },
-
-
 };
